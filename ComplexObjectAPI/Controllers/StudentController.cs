3f1a905cbc5a55ede4d398dbf3a82221fc92eb2b
HemanthKumarBooragam <bhemanthkumar19@gmail.com>
2024-10-21 23:42:14 +0530

    Add project files.

---
+++ ComplexObjectAPI/Controllers/StudentController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ComplexObjectAPI.Models;
+
+namespace ComplexObjectAPI.Controllers
+{
+    [ApiController]
+    [Route("api/student")]
+    public class StudentController
+    {
+        [HttpGet("details")]
+        public List<Student> GetStudent()
+        {
+            return new List<Student>() {
+                    new Student{ID = 1001, Name = "Hemanth", Email = "test@test.com", Address = "Hyderabad, India"},
+                    new Student{ID = 1002, Name = "Surendra", Email = "test@test.com", Address = "Bangalore, India"},
+                    new Student{ID = 1003, Name = "Shravan", Email = "test@test.com", Address = "Andhrapradesh, India"}
+            };
+        }
+    }
+}
---
+++ ComplexObjectAPI/Models/Student.cs
@@ -0,0 +1,57 @@
+﻿namespace ComplexObjectAPI.Models
+{
+    public class Student
+    {
+		private int id;
+        public int ID
+        {
+            get
+            {
+                return id;
+            }
+            set
+            {
+                id = value;
+            }
+        }
+
+        private string name;
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+
+        private string email;
+        public string Email
+        {
+            get
+            {
+                return email;
+            }
+            set
+            {
+                email = value;
+            }
+        }
+
+        private string address;
+        public string Address
+        {
+            get
+            {
+                return address;
+            }
+            set
+            {
+                address = value;
+            }
+        }
+	}
+}
